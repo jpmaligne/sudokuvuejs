ca3764a753502f44f432cd9f0deb811c874c01c7
Jean-Philippe Maligne <jean-philippe@weenect.com>
2023-04-12 18:17:34 +0200

    Init app and Grid

---
+++ src/views/PlayView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+  import { useSudokuGridStore } from '@/stores/grid'
+
+  const gridStore = useSudokuGridStore()
+  gridStore.initGrid()
+  console.log(gridStore.grid);
+</script>
+
+<template>
+  <div class="about">
+    <h1>Good luck</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
