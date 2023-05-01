661dafa2c3e4c59e2ebedc2726fc622d1bc515b3
Jean-Philippe Maligne <jean-philippe@weenect.com>
2023-05-01 21:08:17 +0200

    Diffculty picker and timer

---
+++ src/components/game/Timer.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  import { useGameStore } from '@/stores/game.store'
+
+  const gameStore = useGameStore()
+</script>
+
+<template>
+  <div>
+    <span>{{ gameStore.playTime }}</span>
+  </div>
+</template>
